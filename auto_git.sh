a58f13dd2a5974a1b954a455eb4a330e1aa0ae2e
Ishikawa <bp19076@oli004.sic.shibaura-it.ac.jp>
2019-07-08 17:52:40 +0900

    fire

--- auto_git.sh
+++ auto_git.sh
@@ -6,8 +6,10 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-cp ~/eip1/loader.sh loader.sh
-cp ~/eip1/build.sh build.sh
+cp ~/eip1/loader.sh ~/gitrepo/GodShell
+cp ~/eip1/build.sh ~/gitrepo/GodShell
+
+cd ~/gitrepo/GodShell
 
 git fetch
 git rebase origin/$1
