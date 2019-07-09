d6ce1a12def80bd9029b0c93a2ae22e65bec141f
warosusou <warosusou@gmail.com>
2019-07-09 14:48:49 +0900

    シェルと出力先のディレクトリが別の場所にあっても動作するように変更

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "Error: Not Enough Parameter "
     exit 1
 fi
-cd ./$1
+cd $3$1
 gcc -Wall $2.c -o $2.out
 
 echo "-----Compile end-----"
--- loader.sh
+++ loader.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "error: Not Enough Parameter"
+if [ $# -ne 2 -a $# -ne 3 ]; then
+    echo "error: Unexpected Parameter"
+    echo $#
     exit 1
 fi
 
+if [ $# -eq 2 ]; then
+    Assign_NUM=$1
+    Question_NUM=$2
+    Target_DIR=~/eip1/
+fi
+
+if [ $# -eq 3 ]; then
+    Assign_NUM=$2
+    Question_NUM=$3
+    Target_DIR=$1
+fi
+
+Working_DIR=$Target_DIR$Assign_NUM
 USERNAME=$(whoami)
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ ! -d $1 ]; then
-    mkdir $1
+if [ ! -d $Working_DIR ]; then
+    mkdir $Working_DIR
 fi
-cd $1
-if [ ! -f ${2}.c ]; then
+cd $Working_DIR
+if [ ! -f ${Question_NUM}.c ]; then
     cd ${SCRIPT_DIR}
-    ./template.sh $1 $2
-    cd $1
+    ./template.sh $Assign_NUM $Question_NUM $Working_DIR
+    cd $Working_DIR
 else
     echo "File already exsists..."
 fi
 
-  ALIVE=`ps -ef | grep $USERNAME | grep emacs | grep ${2}.c | wc -l`
+  ALIVE=`ps -ef | grep $USERNAME | grep emacs | grep ${Question_NUM}.c | wc -l`
 
 if [ $ALIVE -eq 0 ]; then
     echo "Starting Emacs"
-    emacs $2.c &
+    emacs $Question_NUM.c &
     PROCESS_ID=$!
 else
-    PROCESS_ID=`ps -ef | grep $USERNAME | grep emacs | grep ${2}.c | cut -f 3 -d " "`
+    PROCESS_ID=`ps -ef | grep $USERNAME | grep emacs | grep ${Question_NUM}.c | cut -f 3 -d " "`
     echo "Emacs already started"
 fi
 
@@ -40,13 +54,13 @@ do
 	break;
     elif [ "$DATA" = "build" ]; then
 	cd ${SCRIPT_DIR}
-	bash build.sh "${1}" "${2}"
-	cd $1
+	bash build.sh "${Assign_NUM}" "${Question_NUM}" "${Working_DIR}"
+	cd $Working_DIR
     elif [ "$DATA" = "emacs" ]; then
-	ALIVE=`ps -ef | grep $USERNAME | grep emacs | grep ${2}.c | wc -l`
+	ALIVE=`ps -ef | grep $USERNAME | grep emacs | grep ${Question_NUM}.c | wc -l`
 	if [ $ALIVE -eq 0 ]; then
 	    echo "Starting Emacs"
-	    emacs $2.c &
+	    emacs $Question_NUM.c &
 	    PROCESS_ID=$!
 	else
 	    echo "Emacs already started"
