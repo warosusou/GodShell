40ab16431c2aa29f7d9dde96e13ecf3affa950a6
ishikawa-taro <taakunn.minecraft@gmail.com>
2019-10-08 12:15:45 +0900

    templateを提出日自動変更システムに対応

--- template2.sh
+++ template2.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-    echo "error: Not Enough Parameter"
+    echo "Error: Not Enough Parameter"
     exit 1
 fi
+
 cd $3
 
-echo "//課題番号"${1}" 問題番号"${2}" 提出日 "" 学籍番号 名前" >> $2.c
-echo "" >> $2.c
-echo "#include <stdio.h>" >> $2.c
-echo "" >> $2.c
-echo "int main(void){" >> $2.c
+echo "/*" >> $2.c
+echo "課題"$1 >> $2.c
+echo "問"$2 >> $2.c
+echo "提出日" >> $2.c
+echo "学籍番号" >> $2.c
+echo "氏名" >> $2.c
+echo "*/" >> $2.c
+echo >> $2.c
+echo "#include<stdio.h>" >> $2.c
+echo >> $2.c
+echo "int main(){" >> $2.c
+echo "  " >> $2.c
 echo "  " >> $2.c
 echo "  " >> $2.c
 echo "  return 0;" >> $2.c
+echo "  " >> $2.c
 echo "}" >> $2.c
+
 exit 0
