77aa446b5247b9dc1cf6b07506003234de7c838b
warosusou <warosusou@gmail.com>
2019-11-05 10:53:44 +0900

    設定ファイル読み込み機能を実装

--- template2.sh
+++ template2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 5 ]; then
     echo "Error: Not Enough Parameter"
     exit 1
 fi
 
-cd $3
+cd $5
 
 echo "/*" >> $2.c
 echo "課題"$1 >> $2.c
 echo "問"$2 >> $2.c
 echo "提出日" >> $2.c
-echo "学籍番号" >> $2.c
-echo "氏名" >> $2.c
+echo "$4" >> $2.c
+echo "$3" >> $2.c
 echo "*/" >> $2.c
 echo >> $2.c
 echo "#include<stdio.h>" >> $2.c
