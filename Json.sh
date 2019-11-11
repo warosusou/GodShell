a56cbef5ab08ea66fb7ba5148bd978c6a6c811eb
ishikawa-taro <taakunn.minecraft@gmail.com>
2019-11-11 14:08:34 +0900

    スペースまでエスケープされていたバグの修正

--- Json.sh
+++ Json.sh
@@ -2,7 +2,7 @@
 
 function JsonReader {
     local formattedjson=$(printf "$1" | sed 's/^{\(.*\)}$/\1/' | tr ',' '\n')
-    local formattedinput=$(printf -- "$2" | sed 's/\(\W\)/\\\1/g')
+    local formattedinput=$(printf -- "$2" | sed 's/\([^a-zA-Z0-9 ]\)/\\\1/g')
     local result=$(printf "$formattedjson" | grep -w "${formattedinput}" | tr -d '"' | sed 's/^.+: //' )
     result=$(printf "$result" | sed "s/^ //"| sed "s/^${formattedinput}: //" )
     echo "$result"
