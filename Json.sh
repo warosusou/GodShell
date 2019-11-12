59b91f799a1fb03ada721504054ad3da34fc2fd3
warosusou <warosusou@gmail.com>
2019-11-12 11:00:16 +0900

    JsonWriterを実装

--- Json.sh
+++ Json.sh
@@ -7,3 +7,23 @@ function JsonReader {
     result=$(printf "$result" | sed "s/^ //"| sed "s/^${formattedinput}: //" )
     echo "$result"
 }
+
+function JsonKeyFinder {
+    local formattedjson=$(printf "$1" | sed 's/^{\(.*\)}$/\1/' | tr ',' '\n')
+    local targetline=$(printf "$formattedjson" | grep $2 | sed 's/^\s//')
+    echo "$targetline"
+}
+
+function JsonWriter {
+    local json="$1"
+    local key=$2
+    local value=$3
+    local result=""
+    local exsistvalue=$(jsonKeyFinder "$json" $key)
+    if [ "$exsistvalue" = "" ]; then
+	result=$(printf "$json" | sed "/.*/N;s/\n}$/,\n \"$key\": \"$value\"\n}/")
+    else
+	result=$(printf "$json" | sed "s/\"$key\":\s\".*\"/\"$key\": \"$value\"/")
+    fi
+    echo "$result"
+}
