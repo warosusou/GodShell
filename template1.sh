6cfcf41c59bb0265144b85b04d2c016e8f6bf49c
warosusou <warosusou@gmail.com>
2019-11-18 13:47:44 +0900

    templateをヒアドキュメント化

--- template1.sh
+++ template1.sh
@@ -4,15 +4,19 @@ if [ $# -ne 5 ]; then
     echo "error: Not Enough Parameter"
     exit 1
 fi
+
 cd $5
 
-echo "//課題番号"${1}" 問題番号"${2}" 提出日 " ${3} ${4} >> $2.c
-echo "" >> $2.c
-echo "#include <stdio.h>" >> $2.c
-echo "" >> $2.c
-echo "int main(void){" >> $2.c
-echo "  " >> $2.c
-echo "  " >> $2.c
-echo "  return 0;" >> $2.c
-echo "}" >> $2.c
+$(cat <<EOF >> $2.c
+//課題番号${1} 問題番号${2} 提出日 ${3} ${4}
+
+#include <stdio.h>
+
+int main(void){
+  
+  
+  return 0;
+}
+EOF
+)
 exit 0
