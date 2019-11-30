902c4d1edd475368e16fa5e4ab6b3797da789dd7
ishikawa-taro <taakunn.minecraft@gmail.com>
2019-11-30 12:18:57 +0900

    randの仮実装

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+. Dictionary.sh
+
+dumpdir="./.dmpfile"
+
 function arrayFilter () {
     local filter="$1"
     local -a array=("${@:2}")
@@ -38,20 +42,10 @@ function Disassembler () {
     do
 	local word="${String:i:len}"
 	local next_escape="$(printf "${String:$i+$len:1}" | sed 's#\([\\|\*|\+|\.|\?|\{|\}|\(|\)|\/|\^|$|\|]\)#\\\1#g' | sed 's#\[#\\\[#g' | sed 's#\]#\\\]#g')"
-	((++len))
-	if [ "$next_escape" = "\(" ]; then
-	    while [ "${next_escape}" != "\)" ];
-	    do
-		word=${word}${next_escape}
-		if [ $(expr $i + $len) -gt ${#String} ]; then return 1; fi
-		local next_escape="$(printf "${String:$i+$len:1}" | sed 's#\([\\|\*|\+|\.|\?|\{|\}|\(|\)|\/|\^|$|\|]\)#\\\1#g' | sed 's#\[#\\\[#g' | sed 's#\]#\\\]#g')"
-		((++len))
-	    done
-	else
-	    if [ "$next_escape" != " " -a $(expr $i + $len) -lt ${#String} ]; then
-		((++len))
-		continue
-	    fi
+	
+	if [ "$next_escape" != " " -a $(expr $i + $len) -lt ${#String} ]; then
+	    ((++len))
+	    continue
 	fi
 	
 	printf "${word}@"
@@ -61,20 +55,63 @@ function Disassembler () {
     printf "\n"
 }
 
+function rand () {
+
+    local dumpfile="${dumpdir}/.randdmp"
+    
+    if [ $# -eq 3 ]; then
+	local -i row=1
+	local -i num=$1
+	local -i min=$2
+	local -i max=$3
+    elif [ $# -eq 4 ]; then
+	local -i row=$1
+	if [ $row -lt 1 ]; then return; fi
+	local -i num=$2
+	local -i min=$3
+	local -i max=$4
+    else
+	echo "BAN"; return 1
+    fi
+
+    if [ $num -lt 1 ]; then return; fi
+    if [ $min -gt $max ]; then return; fi
+
+    local -i i
+
+    if [ ! -d "$dumpdir" ]; then
+	mkdir "$dumpdir"
+    fi
+    
+    if [ -f "$dumpfile" ]; then
+	rm "$dumpfile"
+    fi
+
+    for i in `seq $row`; do
+	for u in `seq $num`; do
+	    printf "%d" $(($min + $RANDOM % ($max - $min + 1))) >> $dumpfile
+	    if [ $u -ne $num ]; then printf " "  >> $dumpfile; fi
+	done
+	if [ $i -ne $row ]; then printf "\n" >> $dumpfile; fi
+    done
+    cat $dumpfile
+}
+
 function Analysis () {
-    local -a arr
-    local str="$(printf "$1" | tr -s ' ')"
-    local IFSbuff="$IFS"
-    IFS="@"
-    #arr=($(Disassembler "$str"))
-    Disassembler "$str"
-    IFS="$IFSbuff"
-    echo "${arr[@]}"
+    local arr=$(eval echo "$@")
+   # local str="$(printf "$1" | tr -s ' ')"
+   # local IFSbuff="$IFS"
+   # IFS="@"
+   # arr=($(Disassembler "$str"))
+   # #Disassembler "$str"
+   # IFS="$IFSbuff"
+    arr=$(printf "$arr" | sed -e 's/\n */\\n/')
+    echo -e "${arr}"
 }
 
 Config_File='./JsonFile/config.json'
 
-json=$(cat $Config_File)
+#json=$(cat $Config_File)
 
 #echo "$json"
 
@@ -82,9 +119,55 @@ json=$(cat $Config_File)
 
 #arrayFilter "ba" "bash ban bil /a -a ./* *"
 
-str="rand(3,3,3) 3 3"
+declare -a command
 
-arr=($(Analysis "$str"))
+ReadInput "BAN?"; str="$BUFF"
 
-echo "${#arr[@]}"
-echo "${arr[@]}"
+#str='`rand 2 5 0 9` 000 00 `rand 2 2 0 9` `rand 2 2 0 9`'
+
+while [ -n "$str" ]; do
+    if [ "${str:0:1}" = "\`" ]; then
+	command+=("$(printf "$str" | sed -E "s/^(\`[^\`]*\`).*/\1/")")
+	str=$(printf "$str" | sed -E "s/^(\`[^\`]*\`)//" )
+    else
+	command+=("$(printf "$str" | sed -E "s/^(^[^ ]*).*/\1/")")
+	str=$(printf "$str" | sed -E "s/^[^ ]*//")
+    fi
+    str=${str## }
+done
+
+declare -a result=()
+
+for cmd in "${command[@]}"
+do
+    declare -i count=$((${#result[@]}-1))
+    if [ $count -lt 0 ]; then count=0; fi
+    if [ "${cmd:0:1}" = "\`" ]; then
+	eval echo "$cmd" > /dev/null
+	#IFSbuff="$IFS"
+	#IFS=$'\n'
+	#result+=($(cat ./.randdmp))
+	#IFS="$IFSbuff"
+	while read line || [ -n "${line}" ]
+	do
+	    result[$count]+="$line"
+	    ((count++))
+	done < ${dumpdir}/.randdmp
+	result[$(($count-1))]+=" "
+    else
+	result[$count]+="$cmd "
+    fi	
+done
+
+for i in $(seq 0 $((${#result[@]}-1)))
+do
+    result[$i]=${result[$i]%% }
+done
+
+for i in "${result[@]}"
+do
+    echo "$i"
+done
+
+
+#rand 2 5 0 9
